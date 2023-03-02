2bd98f1d1aef03a742785f15de90a2e69675b8fe
15Carmen <carmen.martin@iesnervion.es>
2023-03-02 14:38:34 +0100

    actividad 3 empezada

--- src/ejerciciosString2/ejercicio03.java
+++ src/ejerciciosString2/ejercicio03.java
@@ -1,5 +1,7 @@
 package ejerciciosString2;
 
+import java.util.Scanner;
+
 public class ejercicio03 {
 
     /**
@@ -10,6 +12,18 @@ public class ejercicio03 {
      * NOTA: Puedes hacer uso de las funciones estáticas toLowerCase y toUpperCase definidas en la clase Character.
      */
     public static void main(String[] args) {
+        //Declaración de variables
+        String frase;
+
+        //Declaramos el scanner
+        Scanner sc = new Scanner(System.in);
+
+        //Le pedimos al usuario que introduzca una frase
+        System.out.println("Introduzca una frase: ");
+        frase = sc.nextLine();
+
+        //Ponemos la frase en minúsculas
+        frase = frase.toLowerCase();
 
     }
 }
